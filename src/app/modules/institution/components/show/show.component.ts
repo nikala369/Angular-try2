d69867eae5234d2cc1a753a4793cb66eaa95d1d4
Nikala <mikeladzenikala@gmail.com>
2021-04-12 16:08:03 +0400

    add branch data grid

--- src/app/modules/institution/components/show/show.component.ts
+++ src/app/modules/institution/components/show/show.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BranchService } from '../../modules/branch/services/branch.service';
 import { InstitutionService } from '../../services/institution.service';
 
 @Component({
@@ -7,8 +10,41 @@ import { InstitutionService } from '../../services/institution.service';
   styleUrls: ['./show.component.scss'],
 })
 export class ShowComponent implements OnInit {
-  constructor(institutionService: InstitutionService) {}
+  public gridBranchData: any[] = [];
 
-  ngOnInit() {}
-  
+  public showOptions: Array<string> = ['ნახვა', 'რედაქტირება'];
+
+  constructor(
+    public institutionService: InstitutionService,
+    public branchService: BranchService,
+    public router: Router
+  ) {}
+
+  ngOnInit() {
+    this.branchService.getBranches().subscribe(
+      (data: any) => {
+        console.log(data);
+        this.gridBranchData = data.data;
+      },
+      (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            console.log(err);
+          }
+        }
+      }
+    );
+  }
+
+  onItemClick(item: any, dataItem: any) {
+    if (item === 'რედაქტირება') {
+      console.log(item, dataItem);
+      this.institutionService.institutionSubject.next(dataItem);
+      this.router.navigate(['/institutions', dataItem.id, 'edit']);
+    } else if (item === 'ნახვა') {
+      console.log(item, dataItem);
+      this.institutionService.institutionSubject.next(dataItem);
+      this.router.navigate(['/institutions', dataItem.id, 'show']);
+    }
+  }
 }
--- src/app/modules/institution/modules/branch/services/branch.service.ts
+++ src/app/modules/institution/modules/branch/services/branch.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { mainUrl } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BranchService {
+  constructor(private https: HttpClient) {}
 
-  constructor() { }
+  getBranches(): Observable<any> {
+    return this.https.get<any>(`${mainUrl}/institutions/1/branches`);
+  }
 }
